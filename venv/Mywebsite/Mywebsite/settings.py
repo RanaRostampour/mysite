866a73cb3f574d21f7713354a56bf88d55dc56be
raana <Rana@mx>
2021-08-05 12:13:27 +0430

    frontend improved

--- venv/Mywebsite/Mywebsite/settings.py
+++ venv/Mywebsite/Mywebsite/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -55,7 +56,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Mywebsite.urls'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 TEMPLATES = [
     {
 
@@ -124,4 +130,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
