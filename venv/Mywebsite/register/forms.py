aaf0f1cb123ff11d4bacb8b9084d735d82dbc296
raana <Rana@mx>
2021-08-13 13:29:02 +0430

    final check

--- venv/Mywebsite/register/forms.py
+++ venv/Mywebsite/register/forms.py
@@ -7,15 +7,32 @@ from django.forms.widgets import NumberInput, PasswordInput, TextInput
 
 
 class RegisterForm(UserCreationForm):
+    courses= {
+    ('LPIC1', 'LPIC1'),
+    ('LPIC2', 'LPIC2'),
+    ('network+', '+NETWORK'),
+    ('DOCKER', 'DOCKER'),
+    ('Laravel','Laravel'),
+    ('PHP','PHP'),
+    ('Git & Jenkins','Git & Jenkins'),
+    ('Puppet','Puppet'),
+    ('SaltStack','SaltStack')
+    }
+    
     username=forms.CharField(help_text=None,label="نام کاربری")
     password1 = forms.CharField(widget=PasswordInput,label=" گذر واژه ",help_text=None,error_messages='' )
     password2 = forms.CharField(widget=PasswordInput,label="تایید گذر واژه",help_text=None,error_messages='')
     Textarea=forms.TextInput()
-    discord_id = forms.CharField(max_length=100, label='شماره تماس ')
-
+    discord_id = forms.IntegerField( label='شماره تماس ')
+    widget=forms.MultipleChoiceField(
+        label="دوره ها",
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=courses
+    )
 
     
     class Meta:
         model = User
-        fields = ["username", "email","password1","password2","discord_id"]
+        fields = ["widget","username", "email","password1","password2","discord_id"]
 
